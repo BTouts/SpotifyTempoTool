08b9aa14ac2742c3f09d2ff90ba8c91b336bef14
Brett Toutkoushian <btoutkoushian@manh.com>
2023-11-02 23:07:11 -0600

    Made some changes :)

--- main.py
+++ main.py
@@ -2,25 +2,33 @@
 import cred
 import spotify_functions
 import random
+import time
 def driver():
     # Initialize Spotify client
     sp = spotify_functions.create_spotify_client(cred)
-
+    print("Welcome to the Spotify Tempo Tool! This is a tool to take a spotify playlist and queue up a few songs within a tempo range")
+    print()
+    time.sleep(3)
+    print("Please ensure the playlist you are using is not too long, as too many songs can overwhelm the system :(")
+    print()
+    time.sleep(3)
     input("To begin, open Spotify and shuffle the playlist you would like to play songs from, then hit ENTER")
-
+    print()
     bpm = input("Enter the BPM you want to play: ")
-    songs = input("Enter the number of songs you would like to practice with")
+    songs = input("Enter the number of songs you would like to practice with: ")
 
     # Get the URI of the currently playing playlist
     id = sp.currently_playing()['context']['uri']
-    print(id)
-
+    print("Received playlist, currently searching for songs around " + bpm + " bpm!")
+    print()
     # Get filtered tracks from the playlist based on BPM
-    filtered_tracks = spotify_functions.get_filtered_tracks(sp, id, bpm)
-    print("filtered list")
+    filtered_tracks = spotify_functions.get_filtered_tracks(sp, id, bpm,songs)
+    print("Playlist has been filtered, shuffling " + songs + " songs out of " + str(len(filtered_tracks)) + " found!")
+    print()
     playback(sp, filtered_tracks,bpm, songs)
 
-    print("started playback")
+    print()
+    print("All songs have been queued, thank you!")
 def playback(sp, tracklist, bpm, songs):
     counter1 = 0
 
@@ -33,12 +41,14 @@ def playback(sp, tracklist, bpm, songs):
         selected_track = random.choice(tracklist)
 
 
-        print(selected_track['artists'][0]['name'], " – ", selected_track['name'], " - ", int(bpm))
+        print(selected_track['artists'][0]['name'], " – ", selected_track['name'])
+
         sp.add_to_queue(selected_track['id'], device_id=None)
 
         if (counter1 == 0):
             sp.next_track()
-
+        else:
+            time.sleep(10)
         # Remove the selected track from the list to prevent duplicates
         tracklist.remove(selected_track)
 
--- spotify_functions.py
+++ spotify_functions.py
@@ -1,9 +1,8 @@
-# spotify_functions.py
-
-import random
-import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import random
+import time
+
 
 def create_spotify_client(cred):
     return spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -12,6 +11,33 @@ def create_spotify_client(cred):
         redirect_uri=cred.redirect_url,
         scope=cred.SPOTIFY_SCOPE
     ))
+
+
+def get_filtered_tracks(sp, playlist_id, bpm, num_songs, delay=2):
+    # Get all tracks from the playlist
+    tracks = get_playlist_tracks(sp, playlist_id)
+
+    # Filter tracks by BPM
+    filtered_tracks = []
+    count = 0
+    for t in range((len(tracks))):
+        if(count == num_songs):
+            break
+        track = random.choice(tracks)
+        if 'track' in track:
+            track_info = track['track']
+            if track_info['id']:
+                track_bpm = int(sp.audio_features(track_info['id'])[0].get('tempo'))
+                if abs(track_bpm - int(bpm)) <= 5:
+                    filtered_tracks.append(track_info)
+                    count = count + 1
+
+    # Introduce a delay between API calls to stay within rate limits
+    for _ in filtered_tracks:
+        time.sleep(delay)
+
+    return filtered_tracks
+
 
 def get_playlist_tracks(sp, playlist_id):
     tracks = []
@@ -26,24 +52,4 @@ def get_playlist_tracks(sp, playlist_id):
         offset += limit
 
     return tracks
-def get_filtered_tracks(sp, playlist_id, bpm):
-    # Get all tracks from the playlist
-    tracks = get_playlist_tracks(sp, playlist_id)
-
-    # Filter tracks by BPM
-    filtered_tracks = []
-
-    for track in tracks:
-        if 'track' in track:
-            track_info = track['track']
-            if track_info['id']:
-                filtered_tracks.append(track_info)
-
-    # Further filter by BPM in Python (rather than making additional API calls)
-    filtered_tracks = [
-        track for track in filtered_tracks
-        if abs(int(sp.audio_features(track['id'])[0].get('tempo')) - int(bpm)) <= 5
-    ]
-
-    return filtered_tracks
 
